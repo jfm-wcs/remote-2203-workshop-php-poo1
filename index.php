f82e0783de65e561a2354c205e659e62b6c3d501
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-03-29 12:05:26 +0200

    correction de l'atelier

--- index.php
+++ index.php
@@ -2,3 +2,37 @@
 
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+require_once './src/Fighter.php';
+
+$heracles = new Fighter("🧔 Héraclès", 20, 6);
+$lion = new Fighter("🦁 Lion de Némée", 11, 13);
+
+echo $heracles->getName() . " 💙 " . $heracles->getLife() . PHP_EOL;
+echo $lion->getName() . " 💙 " . $lion->getLife() . PHP_EOL;
+
+$round = 1;
+while ($heracles->isAlive() && $lion->isAlive()) {
+    echo "Round #" . $round . PHP_EOL;
+
+    echo $heracles->fight($lion) . PHP_EOL;
+    if ($lion->isAlive()) {
+        echo $lion->fight($heracles) . PHP_EOL;
+    }
+
+    $round++;
+}
+
+// if ($heracles->isAlive()) {
+//     $winner = $heracles;
+//     $looser = $lion;
+// } else {
+//     $winner = $lion;
+//     $looser = $heracles;
+// }
+
+$winner = $heracles->isAlive() ? $heracles : $lion;
+$looser = $heracles->isAlive() ? $lion : $heracles;
+
+echo "💀 " . $looser->getName() . " is dead" . PHP_EOL;
+echo "🏆 " . $winner->getName() . " wins (💙 " . $winner->getLife() .")" . PHP_EOL;
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,90 @@
 <?php
 
 /* Fighter class definition */
+
+class Fighter
+{
+    public const MAX_LIFE = 100;
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life = self::MAX_LIFE;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of strength
+     */ 
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    /**
+     * Get the value of dexterity
+     */ 
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+
+    /**
+     * Get the value of life
+     */ 
+    public function getLife(): int
+    {
+        return $this->life;
+    }
+
+    /**
+     * Set the value of life
+     *
+     * @return  self
+     */ 
+    public function setLife(int $life): self
+    {
+        $this->life = $life;
+
+        return $this;
+    }
+
+    public function fight(Fighter $target): string
+    {
+        $strengthOfFight = rand(1, $this->strength);
+        $damages = $strengthOfFight - $target->getDexterity();
+        if ($damages < 0) {
+            $damages = 0;
+        }
+        // $currentTargetLife = $target->getLife();
+        // $newTargetLife = $currentTargetLife - $damages;
+        // if ($newTargetLife < 0) {
+        //     $newTargetLife = 0;
+        // }
+        // $target->setLife($newTargetLife);
+        $target->setLife(($target->life - $damages) > 0 ? $target->getLife() - $damages : 0);
+        return $this->getName() . " attaque 🗡  💙 " . $target->getName() . ": " . $target->getLife();
+    }
+
+    public function isAlive(): bool
+    {
+        // if ($this->life > 0) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return $this->life > 0;
+    }
+}
